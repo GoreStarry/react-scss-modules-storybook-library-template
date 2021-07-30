5f9bb8b17a6c040fd0543352569544f58258f720
GoreStarry <sunrise91.t3@gmail.com>
2021-07-30 16:47:20 +0800

    [update] rollup config for script and production

--- rollup.config.js
+++ rollup.config.js
@@ -8,37 +8,62 @@ import json from "@rollup/plugin-json";
 import { terser } from "rollup-plugin-terser";
 import babel from "rollup-plugin-babel";
 import del from "rollup-plugin-delete";
+import replace from "rollup-plugin-replace";
 
 const packageJson = require("./package.json");
 
-export default {
-  input: "src/lib.js",
-  output: [
-    {
-      file: packageJson.main,
-      format: "cjs",
-      sourcemap: true,
-    },
-    {
-      file: packageJson.module,
-      format: "esm",
-      sourcemap: true,
-    },
-  ],
+const defaultSettings = {
   plugins: [
-    peerDepsExternal(),
+    // peerDepsExternal(),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    }),
     resolve(),
-    commonjs(),
+    commonjs({
+      // include: /node_modules/
+    }),
     babel({
       exclude: "node_modules/**",
     }),
-    del({ targets: ["dist/*"] }),
+    // del({ targets: ["dist/*"] }),
     postcss({
       modules: true,
       extract: false,
+      // writeDefinitions: true,
+      // inject: true,
     }),
     image(),
     json(),
     terser(),
   ],
 };
+
+export default [
+  {
+    input: "src/lib.js",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    ...defaultSettings,
+  },
+  // {
+  //   input: "src/script.js",
+  //   output: [
+  //     {
+  //       file: "dist/gltfDisplayer.js",
+  //       format: "iife", // for header script
+  //       sourcemap: true,
+  //     },
+  //   ],
+  //   ...defaultSettings,
+  // },
+];
