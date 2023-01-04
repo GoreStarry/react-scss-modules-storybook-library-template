51e590314b10cb05f3bcf10a1544f375be163beb
GoreStarry <sunrise91.t3@gmail.com>
2023-01-04 17:01:44 +0800

    [update] test ver

--- src/examples/Button.vue
+++ src/examples/Button.vue
@@ -1,10 +1,9 @@
 <template>
-  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }}</button>
+  <my-button v-bind="label"></my-button>
 </template>
 
 <script>
-import './button.css';
-import { reactive, computed } from 'vue';
+import MyButton from "./Button.jsx";
 export default {
   name: 'my-button',
   props: {
@@ -12,33 +11,11 @@ export default {
       type: String,
       required: true,
     },
-    primary: {
-      type: Boolean,
-      default: false,
-    },
-    size: {
-      type: String,
-      validator: function (value) {
-        return ['small', 'medium', 'large'].indexOf(value) !== -1;
-      },
-    },
-    backgroundColor: {
-      type: String,
-    },
   },
   emits: ['click'],
   setup(props, { emit }) {
     props = reactive(props);
     return {
-      classes: computed(() => ({
-        'storybook-button': true,
-        'storybook-button--primary': props.primary,
-        'storybook-button--secondary': !props.primary,
-        [`storybook-button--${props.size || 'medium'}`]: true,
-      })),
-      style: computed(() => ({
-        backgroundColor: props.backgroundColor,
-      })),
       onClick() {
         emit('click');
       }
